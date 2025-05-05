621beb9422978d8af63634e7466f111bae9ed2fb
ivolvanov <ivoivanov1605@gmail.com>
2025-05-05 17:51:44 +0200

    start converting to lean4 term syntax

---
+++ bin/gen_lean4.ml
@@ -0,0 +1,186 @@
+open External
+(* open Util *)
+
+let (-->) ty_params ty_body = TyFun (ty_params, ty_body)
+let tInt = TyCons ("Nat", [])
+let tBool = TyCons ("Bool", [])
+let tList ty = TyCons ("List", [ty])
+let tVar a = TyVar a
+
+(* NOTE: no enumFromTo(') or case1 *)
+let lean4_std_lib =
+  [(*("seq",    [tVar "b"; tVar "a"] --> (tVar "a")); *)
+   ("id",     [tVar "a"] --> tVar "a");
+   ("0",      tInt);
+   ("1",      tInt);
+   ("2",      tInt);
+   ("(+)",      [tInt; tInt] --> tInt);
+   ("(-)",      [tInt; tInt] --> tInt);
+   ("Nat.succ", [tInt] --> tInt);
+   (* ("HSub.hSub", [tInt; tInt] --> tInt); *)
+   (* ("List.nil",  tList (tVar "a")); Naturally, this breaks the type inference *)
+   (* ("(:)",    [tVar "a"; tList (tVar "a")] --> tList (tVar "a")); *)
+   ("List.head!",   [tList (tVar "a")] --> tVar "a");
+   ("List.tail",   [tList (tVar "a")] --> tList (tVar "a"));
+   ("List.take",   [tInt; tList (tVar "a")] --> tList (tVar "a"));
+   (* ("(!!)",   [tList (tVar "a"); tInt] --> tVar "a"); *)
+   ("List.length", [tList (tInt)] --> tInt);
+   ("List.or", [tList (tBool)] --> tBool);
+   (*("(++)",   [tList (tVar "a"); tList (tVar "a")] --> tList (tVar "a")); *)
+   (* ("filter", [[tVar "a"] --> tBool; tList (tVar "a")] *)
+              (* --> tList (tVar "a")); *)
+   (*("map",    [[tVar "a"] --> tVar "b"; tList (tVar "a")]
+              --> tList (tVar "b"));
+   ("foldr",  [[tVar "b"; tVar "a"] --> tVar "a";
+               tVar "a";
+               tList (tVar "b")]
+              --> tVar "a"); *)
+   ("Nat.mod",    [tInt; tInt] --> tInt);
+   ("Nat.log2",   [tInt] --> tInt);
+   ("Bool.and",   [tBool; tBool] --> tBool);
+   ("Bool.or",   [tBool; tBool] --> tBool);
+   ("!",    [tBool] --> tBool);
+   ("True",   tBool);
+   ("False",  tBool);
+   ("sorry", tVar "a");
+   (* ("((==)::Int -> Int -> Bool)", [tInt; tInt] --> tBool); *)
+   (* ("((==)::Bool -> Bool -> Bool)", [tBool; tBool] --> tBool); *)
+   (* ("((==)::[Int] -> [Int] -> Bool)", [tList tInt; tList tInt] --> tBool); *)
+  ]
+
+let string_of_ty (ty0 : ty) =
+  let rec lp wr ty =
+    let wrap s =
+      if wr
+      then "(" ^ s ^ ")"
+      else s in
+    match ty with
+    | TyVar _ -> "()"
+    | TyCons ("([])", [ty']) ->
+       "" ^ lp false ty' ^ ""
+    | TyCons (n, tys) ->
+       (match tys with
+        | [] -> n
+        | _ ->
+           wrap (n ^ " " ^ String.concat " " (List.map (lp true) tys)))
+    | TyFun (param_tys, body_ty) ->
+       match param_tys with
+       | [] -> lp wr body_ty
+       | ty' :: tys' ->
+          wrap (lp true ty' ^ " -> " ^ lp false (TyFun (tys', body_ty)))
+    in
+  lp false ty0
+
+let is_infix f =
+  match f with
+  | "(+)" | "(-)"
+    | "(:)" | "(!!)" | "(++)"
+    | "(&&)" | "(||)" -> true
+  | _ -> false
+
+let make_infix f =
+  String.sub f 1 (String.length f - 2)
+
+let rec lean4_string e =
+  match e with
+  | Ref (x, _) ->
+      x
+     (* let x_annot () = "(" ^ x ^ ":" ^ ")" in
+     (match List.assoc_opt x haskell_std_lib with
+      | None -> x
+      | Some ty ->
+         if SS.is_empty (ty_vars ty)
+         then x
+         else x_annot ()) *)
+  | Lambda (xs, e_body) ->
+     (match xs with
+      | [] -> lean4_string e_body
+      | _ ->
+          let annot = String.concat " " 
+          (List.map (fun (name, ty) -> "(" ^ name ^ " : " ^ string_of_ty ty ^ ")") xs) in
+         "(fun " ^ annot ^ " => (" ^ lean4_string e_body ^ "))")
+  | Call (e_f, e_args) ->
+    (match e_f, e_args with
+     | Ref (f, _), [e1; e2] when is_infix f ->
+        "(" ^ lean4_string e1 ^
+        " " ^ make_infix f ^ " " ^
+        lean4_string e2 ^ ")"
+     | _, _ ->
+        match e_args with
+        | [] -> lean4_string e_f
+        | _ ->
+          "(" ^ lean4_string e_f ^ " " ^
+          String.concat " " (List.map lean4_string e_args) ^ ")")
+  | Let ((_, _), _, _) -> raise Util.Unimplemented
+
+let generate_haskell size =
+  let weights x = 
+    match x with
+    | "(!!)" | "[]" -> 1. /. 3.
+    | "head" | "tail" -> 1. /. 4.
+    | "id" | "sorry" -> 1. /. 10.
+    | _ -> 1. in
+  let weighted_std_lib =
+    List.map (fun entry -> (weights (fst entry), entry)) lean4_std_lib in
+  let gen_ty = [tList (tInt)] --> tInt in
+  (* let gen_ty = [tInt; tInt; tInt] --> tInt in *)
+  (* let gen_ty = [tInt] --> tInt in *)
+  Generate.generate_exp weighted_std_lib size tInt gen_ty
+  (* TODO: program stats in debug mode *)
+
+let generate_batch exp_size batch_size =
+  Seq.init batch_size
+           (fun _ ->
+             let p = generate_haskell exp_size in
+             Debug.run prerr_newline;
+             p)
+
+let print_file fs =
+
+  let rec print_lines pre post lines =
+    match lines with
+    | [] -> ()
+    | [l] -> print_string pre; print_endline l
+    | l :: lines' ->
+       print_string pre;
+       print_string l;
+       print_endline post;
+       print_lines pre post lines' in
+
+  let prelude = [
+      "set_option linter.unusedVariables false\n";
+    ] in
+
+  let main = [
+    "-- placeholder" ;
+    ] in
+
+  print_lines "" "" prelude;
+  (* print_endline "codeList :: [[Int] -> [Int]]"; *)
+  (* print_endline "codeList = ["; *)
+  print_lines "#check " "" fs;
+  (* print_endline "  ]"; *)
+  print_lines "" "" main
+
+let n = ref 100
+let size = ref 25
+let seed = ref (-1)
+
+let speclist =
+  [
+    ("-n", Arg.Set_int n, "Number of functions to generate");
+    ("-size", Arg.Set_int size, "Size of each function");
+    ("-seed", Arg.Set_int seed, "Random generator seed");
+    ("-debug", Arg.Set Debug.debug_mode, "Enable debug mode");
+  ]
+
+(* -O -fno-full-laziness *)
+let () =
+  Arg.parse speclist (fun _ -> ())
+    "gen_haskell [-testtype <0>] [-n <100>] [-size <100>] [-seed <-1>";
+  (if !seed < 0
+   then Random.self_init ()
+   else Random.init !seed);
+
+  let fs = Seq.map lean4_string (generate_batch !size !n) in
+  print_file (List.of_seq fs)
